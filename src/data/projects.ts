603b6c17d5de5ec77d5f27845e2f47abdb4e525f
tiago-simionato <tiago.simionato5@gmail.com>
2024-01-02 09:41:52 -0300

    feat: sewlp project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,34 +1,46 @@
 import IProjectItem from "common/interfaces/IProjectItem";
 
-const projects : IProjectItem[] = [
+const projects: IProjectItem[] = [
   {
-    "name": "Glypho",
-    "description": "Glypho Compiler é um projeto de compilador para ser utilizado através da web. É um projeto FullStack que conta com uma interface FrontEnd que se conecta à API Rest do BackEnd para realizar a compilação do código escrito. Sua interface simples e intuitiva possui coloração automática de código utilizando diversas expressões regulares e a compilação pode ser feita para 3 linguagens-algo a escolha: Java, JavaScript ou Python",
-    "imgPath": "assets/images/glypho-interface.webp",
-    "imgAlt": "Interface do Glypho",
-    "externalLink": "https://tiago-simionato.github.io/GlyphoFE/"
+    name: "Soft Everywhere Landing Page",
+    description:
+      "Landing Page feita com AstroJs. Uma página com excelente performance, conta com responsividade e diversas animações que são acionada automaticamente ao descer a página",
+    imgPath: "assets/images/sewlp.webp",
+    imgAlt: "Captura da Página Soft Everywhere",
+    externalLink: "https://landing-page-sew.vercel.app/",
   },
   {
-    "name": "Soft Everywhere Test",
-    "description": "O projeto consiste em uma Landing Page moderna, responsiva e acessível criada a partir de um Figma. A página conta com um carrosel, diversas animações e foi desenvolvida utilizando React e Tailwind CSS",
-    "imgPath": "assets/images/sew.webp",
-    "imgAlt": "Interface do Soft everywhere",
-    "externalLink": "https://frontend-test-tiago.vercel.app/"
+    name: "Glypho",
+    description:
+      "Glypho Compiler é um projeto de compilador para ser utilizado através da web. É um projeto FullStack que conta com uma interface FrontEnd que se conecta à API Rest do BackEnd para realizar a compilação do código escrito. Sua interface simples e intuitiva possui coloração automática de código utilizando diversas expressões regulares e a compilação pode ser feita para 3 linguagens-algo a escolha: Java, JavaScript ou Python",
+    imgPath: "assets/images/glypho-interface.webp",
+    imgAlt: "Interface do Glypho",
+    externalLink: "https://tiago-simionato.github.io/GlyphoFE/",
   },
   {
-    "name": "Alura Geek",
-    "description": "Página desenvolvida durante a quinta edição do desafio de front-end da Alura. Simula a vitrine de uma loja virtual que vende produtos da cultura pop. Desenvolvido apenas com HTML, CSS e JavaScript",
-    "imgPath": "assets/images/alurageek.webp",
-    "imgAlt": "Interface do Alura Geek",
-    "externalLink": "https://tiagoalurageek.vercel.app/"
+    name: "Soft Everywhere Test",
+    description:
+      "O projeto consiste em uma Landing Page moderna, responsiva e acessível criada a partir de um Figma. A página conta com um carrosel, diversas animações e foi desenvolvida utilizando React e Tailwind CSS",
+    imgPath: "assets/images/sew.webp",
+    imgAlt: "Interface do Soft everywhere",
+    externalLink: "https://frontend-test-tiago.vercel.app/",
   },
   {
-    "name": "Organo",
-    "description": "Organo é uma aplicação feita com React onde é possível realizar a organização e exibição de diversos times. Por base são times de desenvolvedores, mas o dinamismo da página permite a criação de novos times, bem como a criação de novos integrantes",
-    "imgPath": "assets/images/organo.webp",
-    "imgAlt": "Interface do Organo",
-    "externalLink": "https://organo-tiago.vercel.app/"
-  }
-]
+    name: "Alura Geek",
+    description:
+      "Página desenvolvida durante a quinta edição do desafio de front-end da Alura. Simula a vitrine de uma loja virtual que vende produtos da cultura pop. Desenvolvido apenas com HTML, CSS e JavaScript",
+    imgPath: "assets/images/alurageek.webp",
+    imgAlt: "Interface do Alura Geek",
+    externalLink: "https://tiagoalurageek.vercel.app/",
+  },
+  {
+    name: "Organo",
+    description:
+      "Organo é uma aplicação feita com React onde é possível realizar a organização e exibição de diversos times. Por base são times de desenvolvedores, mas o dinamismo da página permite a criação de novos times, bem como a criação de novos integrantes",
+    imgPath: "assets/images/organo.webp",
+    imgAlt: "Interface do Organo",
+    externalLink: "https://organo-tiago.vercel.app/",
+  },
+];
 
 export default projects;
