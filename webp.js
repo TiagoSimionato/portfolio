bffc1a2ee6dfc782da0f4ada4621e9e59d5d7afd
tiago-simionato <tiago.simionato5@gmail.com>
2024-01-04 08:10:09 -0300

    feat: gif 2 webp conversion

--- webp.js
+++ webp.js
@@ -1,27 +1,33 @@
-import imagemin from 'imagemin'
-import webp from 'imagemin-webp'
+import imagemin from "imagemin";
+import webp from "imagemin-webp";
+import gif2webp from "imagemin-gif2webp";
 
-const outputFolder = './public/assets/images/'
-const inputFolder = 'public/assets/images/'
+const outputFolder = "./public/assets/images/";
+const inputFolder = "public/assets/images/";
 
 const produceWebP = async () => {
-  await imagemin([inputFolder + '*.png'], {
+  await imagemin([inputFolder + "*.png"], {
     destination: outputFolder,
     plugins: [
       webp({
-        lossless: true
-      })
-    ]
-  })
-  console.log('PNGs processed')
-  await imagemin([inputFolder + '*.{jpg,jpeg}'], {
+        lossless: true,
+      }),
+    ],
+  });
+  console.log("PNGs processed");
+  await imagemin([inputFolder + "*.{jpg,jpeg}"], {
     destination: outputFolder,
     plugins: [
       webp({
-        quality: 65
-      })
-    ]
-  })
-  console.log('JPGs and JPEGs processed')
-}
-produceWebP()
+        quality: 65,
+      }),
+    ],
+  });
+  console.log("JPGs and JPEGs processed");
+  await imagemin([inputFolder + "*.gif"], {
+    destination: outputFolder,
+    plugins: [gif2webp({ lossy: true, minimize: true })],
+  });
+  console.log("Gifs processed");
+};
+produceWebP();
