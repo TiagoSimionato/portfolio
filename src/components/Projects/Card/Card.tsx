11cf025cbc664ed6b16d993f156c600d0e113f1a
tiago-simionato <tiago.simionato5@gmail.com>
2024-01-04 19:32:39 -0300

    fix: card title styling

--- src/components/Projects/Card/Card.tsx
+++ src/components/Projects/Card/Card.tsx
@@ -1,21 +1,21 @@
 import IProjectItem from "common/interfaces/IProjectItem";
 import styled from "styled-components";
-import variables from 'common/variables.json';
+import variables from "common/variables.json";
 import Image from "components/Image";
 
 interface ISCard {
-  active?  : boolean,
-  cardWidth : number,
+  active?: boolean;
+  cardWidth: number;
 }
 
 const StyledCard = styled.div<ISCard>`
-  width: ${props => props.cardWidth}px;
+  width: ${(props) => props.cardWidth}px;
   background-color: ${variables.colors.primaryTransparent};
   box-shadow: 1px 1px 20px -5px rgba(0, 0, 0, 0.7);
   margin: 0.5rem;
   border-radius: 1%;
   height: 100%;
-  
+
   a {
     display: flex;
     flex-direction: column;
@@ -24,17 +24,25 @@ const StyledCard = styled.div<ISCard>`
   }
 
   img {
-    width: ${props => props.cardWidth}px;
+    width: ${(props) => props.cardWidth}px;
     border-radius: 1%;
-    //${props => props.active ? `width: 500px; height: 400px;` : ''} TODO IMPROVE CAROUSEL STYLE
+    /*${(props) =>
+      props.active
+        ? `width: 500px; height: 400px;`
+        : ""} TODO IMPROVE CAROUSEL STYLE*/
   }
 
   .projectName {
     font-weight: 600;
     font-size: 2rem;
-    padding: 1rem 0 0.5rem 0;
+    line-height: 3rem;
+    padding: 1rem 1.5rem 0.5rem 1.5rem;
     margin-bottom: 0.5rem;
-    border-bottom: solid 1px ${variables.colors.contrast};
+    text-align: center;
+    text-decoration: underline;
+    text-decoration-color: ${variables.colors.contrast};
+    text-underline-offset: 13px;
+    text-decoration-thickness: 1px;
   }
 
   .description {
@@ -57,26 +65,26 @@ const StyledCard = styled.div<ISCard>`
     }
   }
 
-  //${props => props.active ? `transform: scale(1.1);` : ''}
-`
+  //${(props) => (props.active ? `transform: scale(1.1);` : "")}
+`;
 
 interface Props extends IProjectItem {
-  active?   : boolean,
-  cardWidth? : number,
+  active?: boolean;
+  cardWidth?: number;
 }
 
-export function Card({ name, description, imgPath, imgAlt, externalLink, cardWidth = 500 } : Props) {
+export function Card({
+  name,
+  description,
+  imgPath,
+  imgAlt,
+  externalLink,
+  cardWidth = 500,
+}: Props) {
   return (
     <StyledCard cardWidth={cardWidth}>
-      <a
-        href={externalLink}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Image
-          src={imgPath}
-          alt={imgAlt}
-        />
+      <a href={externalLink} target="_blank" rel="noreferrer">
+        <Image src={imgPath} alt={imgAlt} />
         <h2 className="projectName">{name}</h2>
         <p className="description">{description}</p>
       </a>
