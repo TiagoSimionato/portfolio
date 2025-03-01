1e438ec40a724a770ad3749373498a08d3f05d53
Tiago Simionato <tiago.simionato5@gmail.com>
2025-03-01 03:23:34 -0300

    feat: about me migration

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,58 +1,30 @@
-import variables from 'common/variables.json';
+import { Stack, Typography } from '@mui/material';
 import Image from 'components/Image';
-import styled from 'styled-components';
-
-const StyledSection = styled.section`
-  display: flex;
-  align-items: center;
-  gap: 2rem;
-  margin: auto;
-  padding: 3.75rem 6.25rem;
-  max-width: 1440px;
-
-  img {
-    height: 220px;
-    width: 220px;
-    border-radius: 100%;
-    box-shadow: 1px 1px 20px -5px rgba(0, 0, 0, 0.7);
-  }
-
-  .container {
-    display: flex;
-    flex-direction: column;
-    gap: 1.5rem;
-  }
-  
-  p {
-    font-weight: 500;
-    line-height: 1.5rem;
-    text-align: justify;
-  }
-
-  @media screen and (max-width: ${variables.breakpoints.tablet}) {
-    flex-direction: column;
-    padding: 3.75rem 3rem;
-
-    .container {
-      order: 1;
-    }
-  }
-
-  @media screen and (max-width: ${variables.breakpoints.tablet}) {
-    padding: 3.75rem 1.5rem;
-  }
-`;
 
 export const AboutMe = () => (
-  <StyledSection id="aboutme">
+  <Stack
+    id="aboutme"
+    sx={{
+      alignItems: 'center',
+      flexDirection: { md: 'row', xs: 'column' },
+      gap: '2rem',
+      margin: 'auto',
+      maxWidth: 1440,
+    }}
+  >
     <Image
       src="assets/images/tiago.jpg"
       alt="Autor da Página"
+      style={{ borderRadius: '100%', boxShadow: '1px 1px 20px -5px rgba(0, 0, 0, 0.7)', height: 220, width: 220 }}
     />
-    <div className="container">
-      <h2 className="secTitle">Sobre mim</h2>
-      <p>Sou desenvolvedor apaixonado por tecnologia, lógica e desafios! Por causa da minha vontade de dar mais vida aos códigos que desenvolvia durante a graduação decidi me especializar em Front End, assim poderia desenvolver aplicações com um grau de contato mais direto com o usuário final. Desde então aprendi a utilizar diversas tecnlogias usadas no Front, sendo meu framework principal o React.js</p>
-      <p>Gosto de estar sempre aprendendo coisas novas e minha principal realização é ver as aplicações que desenvolvo sendo úteis para as pessoas em seu dia a dia.</p>
-    </div>
-  </StyledSection>
+    <Stack sx={{ gap: '1.5rem' }}>
+      <Typography variant="h2">Sobre mim</Typography>
+      <Typography>
+        Sou desenvolvedor apaixonado por tecnologia, lógica e desafios! Por causa da minha vontade de dar mais vida aos códigos que desenvolvia durante a graduação decidi me especializar em Front End, assim poderia desenvolver aplicações com um grau de contato mais direto com o usuário final. Desde então aprendi a utilizar diversas tecnlogias usadas no Front, sendo meu framework principal o React.js
+      </Typography>
+      <Typography>
+        Gosto de estar sempre aprendendo coisas novas e minha principal realização é ver as aplicações que desenvolvo sendo úteis para as pessoas em seu dia a dia.
+      </Typography>
+    </Stack>
+  </Stack>
 );
--- src/theme.ts
+++ src/theme.ts
@@ -10,6 +10,18 @@ export const theme = createTheme({
     },
   },
   typography: {
+    body1: {
+      textAlign: 'justify',
+    },
     fontFamily: poppins.style.fontFamily,
+    h2: {
+      '@media (min-width:800px)': {
+        fontSize: '3rem',
+      },
+      'color': tokens.colors.contrast,
+      'fontSize': '2.275rem',
+      'fontWeight': 600,
+      'lineHeight': '2.75rem',
+    },
   },
 });
