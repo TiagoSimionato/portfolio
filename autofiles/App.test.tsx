3f96be1ddc8e183ac9ed79c2e167ee2adf14770a
Tiago Simionato <tiago.simionato5@gmail.com>
2025-03-06 16:20:28 -0300

    feat: delete unnecessary auto files

--- autofiles/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-// import App from './routes';
-
-it('renders learn react link', () => {
-  render(<h1 />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
