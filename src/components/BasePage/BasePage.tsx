70c3830441c9e5137d385e4bcce0da616a2ea36c
Tiago Simionato <tiago.simionato5@gmail.com>
2025-03-06 12:43:46 -0300

    fix: mobile background

--- src/components/BasePage/BasePage.tsx
+++ src/components/BasePage/BasePage.tsx
@@ -4,24 +4,10 @@ import { Socials } from 'app/components';
 import { Footer, Header } from 'components';
 import { tokens } from 'tokens';
 
-const Background = (
-  <Stack sx={{
-    backgroundImage: `linear-gradient(${tokens.colors.primary},${tokens.colors.secondary})`,
-    bottom: 0,
-    left: 0,
-    position: 'fixed',
-    right: 0,
-    top: 0,
-    zIndex: -1,
-  }}
-  />
-);
-
 export const PAGE_PADDING = { md: '4rem', xs: '1rem' };
 
 export const BasePage = ({ children }: PropsWithChildren) => (
-  <>
-    {Background}
+  <Stack sx={{ backgroundImage: `linear-gradient(${tokens.colors.primary},${tokens.colors.secondary})`, flexGrow: 1 }}>
     <Header />
     <Stack paddingX={PAGE_PADDING} flex={1} justifyContent="center" height="100%">
       <Stack flex={1}>
@@ -30,5 +16,5 @@ export const BasePage = ({ children }: PropsWithChildren) => (
       <Socials />
       <Footer />
     </Stack>
-  </>
+  </Stack>
 );
